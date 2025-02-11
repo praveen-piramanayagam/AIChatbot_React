63d62f455bec96e7f7c4ae165d186aaa83ced0f5
praveen piramanayagam <praveennayagam18@gmail.com>
2025-02-11 18:10:12 +0530

    Server deployed link changes

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://aichatbot-server.onrender.com/api/auth";
 
 export const registerUser = async (username, password) => {
   const response = await axios.post(`${API_URL}/register`, { username, password });
--- src/api/chatApi.js
+++ src/api/chatApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/chatbot";
+const API_URL = "https://aichatbot-server.onrender.com/api/chatbot";
 
 export const sendMessage = async (message, token) => {
   const response = await axios.post(
